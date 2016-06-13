074ee6f4c1bd3f5e9e7caeb6d47c217510480325
Dillon Zhang <d1998z@gmail.com>
2016-06-12 23:47:01 -0400

    Final?

--- gmath.py
+++ gmath.py
@@ -33,3 +33,39 @@ def calculate_dot(points, i, vector=[0, 0, -1], normalize=False):
 
 def magnitude(vector):
     return (vector[0]**2 + vector[1]**2 + vector[2]**2)**(.5)
+
+
+def dot_poduct(v1, v2):
+    return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
+
+
+def normalize(vector):
+    vectorprime = vector[:]
+    for i in range(len(vectorprime)):
+        vectorprime[i] /= magnitude(vectorprime)
+    return vectorprime
+        
+
+def get_normal(points, i=0):
+    ax = points[i + 1][0] - points[i][0]
+    ay = points[i + 1][1] - points[i][1]
+    az = points[i + 1][2] - points[i][2]
+    bx = points[i + 2][0] - points[i][0]
+    by = points[i + 2][1] - points[i][1]
+    bz = points[i + 2][2] - points[i][2]
+    normal = calculate_normal(ax, ay, az, bx, by, bz)
+    return normal
+
+
+def sub_vector(v1, v2):
+    vR = [0, 0, 0]
+    for i in range(3):
+        vR[i] = v1[i] - v2[i]
+    return vR
+
+
+def scalar_mult(v1, scalar):
+    vR = [0, 0, 0]
+    for i in range(3):
+        vR[i] = v1[i] * scalar
+    return vR
